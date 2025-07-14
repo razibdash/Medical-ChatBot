96dd375020deaca52df69e069f25671dc424fd5d
razibdash <avrorazib@gmail.com>
2025-07-14 18:00:12 +0600

    change prompt

--- src/prompt.py
+++ src/prompt.py
@@ -1,5 +1,6 @@
 system_prompt = (
     "You are an assistant for question-answering tasks. "
+    "Your Name is Dr.MCB"
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
     "don't know. Use three sentences maximum and keep the "
